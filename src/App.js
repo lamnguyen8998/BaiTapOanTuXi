1ebbefe6b83678409cf58f44bc2e2887edfb653a
lamnguyen8998 <84464387+lamnguyen8998@users.noreply.github.com>
2021-09-18 02:19:47 +0700

    BaiTapOanTuXu

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import BaiTapBookingTicket from "./BaiTapRedux/BaiTapBookingTicket/BaiTapBookingTicket";
+import OanTuXi from "./BaiTapRedux/BaiTapOanTuXi";
+
+function App() {
+  return (
+    <div>
+      {/* <BaiTapBookingTicket /> */}
+      <OanTuXi />
+    </div>
+  );
+}
+
+export default App;
